621866158c87560f49e2b4db7b5a1eec602fb9b2
ironholds <ironholds@gmail.com>
2016-07-04 14:13:52 -0400

    add package-level docs

--- R/triebeard.R
+++ R/triebeard.R
@@ -1,3 +1,10 @@
+#' @title Radix trees in Rcpp
+#' @name triebeard
+#' @description This package provides access to Radix tree (or "trie") structures in Rcpp. At
+#' a later date it will hopefully provide them in R, too.
+#'
+#' @docType package
+#' @aliases triebeard triebeard-package
 #' @useDynLib triebeard
 #' @importFrom Rcpp sourceCpp
 NULL
