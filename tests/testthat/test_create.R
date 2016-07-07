440e786b4bfb17332adcef6822c55a39c781b158
ironholds <ironholds@gmail.com>
2016-07-06 22:07:01 -0400

    teststeststests

--- tests/testthat/test_create.R
+++ tests/testthat/test_create.R
@@ -2,11 +2,8 @@ context("Test trie creation")
 
 testthat::test_that("String tries can be created and safely avoid collection", {
   string_trie <- create_trie(LETTERS, LETTERS)
-
   testthat::expect_true(any(class(string_trie) == "string_trie"))
   testthat::expect_true(any(class(string_trie) == "trie"))
-  testthat::expect_equal(LETTERS, get_values(string_trie))
-  testthat::expect_equal(LETTERS, get_keys(string_trie))
 })
 
 testthat::test_that("Integer tries can be created", {
@@ -14,9 +11,6 @@ testthat::test_that("Integer tries can be created", {
 
   testthat::expect_true(any(class(int_trie) == "integer_trie"))
   testthat::expect_true(any(class(int_trie) == "trie"))
-  testthat::expect_equal(1:length(LETTERS), get_values(int_trie))
-  testthat::expect_equal(LETTERS, get_keys(int_trie))
-
 })
 
 testthat::test_that("Double tries can be created", {
@@ -25,8 +19,6 @@ testthat::test_that("Double tries can be created", {
 
   testthat::expect_true(any(class(double_trie) == "numeric_trie"))
   testthat::expect_true(any(class(double_trie) == "trie"))
-  testthat::expect_equal(vals, get_values(double_trie))
-  testthat::expect_equal(LETTERS, get_keys(double_trie))
 })
 
 testthat::test_that("Logical tries can be created", {
@@ -35,6 +27,4 @@ testthat::test_that("Logical tries can be created", {
 
   testthat::expect_true(any(class(bool_trie) == "logical_trie"))
   testthat::expect_true(any(class(bool_trie) == "trie"))
-  testthat::expect_equal(vals, get_values(bool_trie))
-  testthat::expect_equal(LETTERS, get_keys(bool_trie))
 })
